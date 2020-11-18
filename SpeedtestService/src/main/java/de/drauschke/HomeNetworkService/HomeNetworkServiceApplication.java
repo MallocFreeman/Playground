ce4056033eb15d8ac11ce138b2b0ba0a385a6487
Thomas Drauschke <thomasdrauschke@gmx.de>
2020-11-18 19:12:04 +0100

    Cleaned Ehcache xml

--- SpeedtestService/src/main/java/de/drauschke/HomeNetworkService/HomeNetworkServiceApplication.java
+++ SpeedtestService/src/main/java/de/drauschke/HomeNetworkService/HomeNetworkServiceApplication.java
@@ -3,9 +3,11 @@ package de.drauschke.HomeNetworkService;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableCaching
 @EnableSwagger2
 @AllArgsConstructor
 @SpringBootApplication
